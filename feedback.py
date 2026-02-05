83a17b03440e9f220756193db4c250135031217b
Sat Garcia <sat@sandiego.edu>
2026-02-04 23:04:53 -0800

    add feedback.py with starting code for practice problem

---
+++ feedback.py
@@ -0,0 +1,17 @@
+"""
+Module: feedback
+
+A program to create individualized feedback files for students
+in a class.
+"""
+
+def individualize_grades(filename):
+    """ To Do: Complete this docstring comment. """
+
+    pass # implement this function and remove this pass statement
+
+def main() -> None:
+    individualize_grades("psa_grades.csv")
+
+if __name__ == "__main__":
+    main()
