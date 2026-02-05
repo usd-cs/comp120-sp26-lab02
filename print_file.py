d5a11b52f6c13ab70894c7c1ade3bc64f9a56fe7
Sat Garcia <sat@sandiego.edu>
2026-02-04 22:09:17 -0800

    add doctest and main function

--- print_file.py
+++ print_file.py
@@ -3,13 +3,23 @@ from sys import argv
 def print_file(filename: str) -> None:
     """Prints out the contents of a file.
 
-    Args:
+    Parameters:
         filename (str): The name of the file to open and print out.
+
+    Examples:
+        >>> print_file('haiku.txt')
+        An old silent pond
+        A frog jumps into the pond-
+        Splash! Silence again.
     """    
     f = open(filename, 'r')
     for line in f:
-        print(line)
+        print(line, end='')
 
 
-print(type(argv))
-print(argv)
+def main() -> None:
+    print(type(argv))
+    print(argv)
+
+if __name__ == "__main__":
+    main()
